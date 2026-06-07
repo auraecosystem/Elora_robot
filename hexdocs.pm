7ef717f788f842fb641c0636abf01a10711ebecc
auraecosystem <sandraisom@icloud.com>
2026-06-07 22:47:19 +0100

    Create hexdocs.pm

---
+++ hexdocs.pm
@@ -0,0 +1,21 @@
+def project do
+  [
+    app: :my_app,
+    version: "0.1.0-dev",
+    deps: deps(),
+
+    # Docs
+    name: "MyApp",
+    source_url: "https://github.com/auraecosystem/Elora_robot",
+    homepage_url: "http://Elora_robot.pm",
+    docs: &docs/0
+  ]
+end
+
+defp docs do
+  [
+    main: "readme", # can be changed to a module name, if you prefer
+    logo: "path/to/logo.png",
+    extras: ["README.md"]
+  ]
+end
